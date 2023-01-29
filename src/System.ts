fd17336fecd6a81095713c8e3cc35ce046b202ad
黑白 <2285697543@qq.com>
2023-01-29 16:22:46 +0800

    输出

--- src/System.ts
+++ src/System.ts
@@ -6,30 +6,29 @@
  */
 module OpenAPI {
 
+  /**
+   * 插件的系统API
+   */
+  export class System {
+
     /**
-     * 插件的系统API
+     * 当前版本号
      */
-    export class System {
+    static Version = 2.0;
 
-        /**
-         * 当前版本号
-         */
-        static Version = 2.0;
+    /**
+     * 是否安装本插件
+     */
+    static Installed = true;
+  }
 
-        /**
-         * 是否安装本插件
-         */
-        static Installed = true;
-
+  // 输出
+  if (typeof Config !== 'undefined' && typeof OpenAPI !== 'undefined') {
+    if (!Config.RELEASE_GAME) {
+      trace(`OpenAPI v${OpenAPI.System.Version.toFixed(1)} => OK`)
+    } else {
+      console.log(` %c OpenAPI v${OpenAPI.System.Version.toFixed(1)} %c https://www.gamecreator.com.cn/plug/det/641 `, "color: #fadfa3; background: #333; padding:8px;border-left:1px solid #fadfa3;border-top:1px solid #fadfa3;border-bottom:1px solid #fadfa3;", "color: #fadfa3; background: #333; padding:8px; border:1px solid #fadfa3;");
     }
-
-    // 输出
-    if (typeof Config !== 'undefined' && typeof OpenAPI !== 'undefined') {
-        if (!Config.RELEASE_GAME) {
-          trace(`OpenAPI v${OpenAPI.System.Version.toFixed(1)} => OK`)
-        } else {
-          console.log(` %c OpenAPI v${OpenAPI.System.Version.toFixed(1)} %c https://www.gamecreator.com.cn/plug/det/641 `, "color: #fadfa3; background: #333; padding:8px;", "color: #111; background: #fadfa3; padding:8px;");
-        }
-      }
+  }
 
 };
