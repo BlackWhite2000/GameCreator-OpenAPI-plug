7cdf01ffc275513b96f40cc076162c8eac0a2485
BlackWhite <2285697543@qq.com>
2024-07-01 08:12:44 +0800

    新增函数

--- src/Method.ts
+++ src/Method.ts
@@ -231,5 +231,238 @@ module OpenAPI {
         return null
       }
     };
+
+    /**
+* 解析文本内函数组合
+* @param {string} text 文本
+*/
+    static parseCombinedFunctions(text: string): string {
+      const getData = [
+        (...args: number[]) => { return Math.max(...args) },
+        (...args: number[]) => { return Math.min(...args) },
+        (a: number, b: number) => {
+          const [min, max] = a < b ? [a, b] : [b, a];
+          return Math.random() * (max - min) + min;
+        },
+        (...args: number[]) => { return args.reduce((acc, val) => acc + val, 0) },
+        (num: number) => { return Math.abs(num) },
+        (num: number) => { return Math.sqrt(num) },
+        (num: number) => { return Math.round(num) },
+      ];
+
+      const regex = [
+        /max\((?:\d+(\.\d+)?(,\s*)?)*\d+(\.\d+)?\)/g,
+        /min\((?:\d+(\.\d+)?(,\s*)?)*\d+(\.\d+)?\)/g,
+        /random\(\d+(\.\d+)?(,\s*)?\d+(\.\d+)?\)/g,
+        /reduce\((?:\d+(\.\d+)?(,\s*)?)*\d+(\.\d+)?\)/g,
+        /abs\(\d+(\.\d+)?\)/g,
+        /sqrt\(\d+(\.\d+)?\)/g,
+        /round\(\d+(\.\d+)?\)/g,
+      ];
+
+      for (let i = 0; i < getData.length; i++) {
+        const result = this.replaceFunctionCombinations(text, regex[i], getData[i]);
+        if (result)
+          text = result;
+      }
+      return text;
+    }
+
+    /**
+       * 替换函数组合数据
+       * @param {string} text 文本
+       * @param {RegExp} regex 正则表达式
+       * @param {any} getData 解析函数组合数据
+       */
+    static replaceFunctionCombinations(text: string, regex: RegExp, getData: any): string | null {
+      const matches = text.match(regex);
+      if (matches) {
+        for (let i = 0; i < matches.length; i++) {
+          const d = matches[i];
+          const argsString = d.slice(d.indexOf('(') + 1, d.indexOf(')'));
+          const args = argsString.split(',').map(Number);
+          if (args.every(arg => !isNaN(arg))) {
+            const v = getData(...args);
+            text = text.replace(d, v.toString());
+          }
+        }
+        return text;
+      } else {
+        return null;
+      }
+    }
+
+    /**
+     * 解析并计算复杂表达式的函数
+     * @param {string} expression - 要解析的表达式
+     * @returns {any} 计算结果，如果表达式无效则返回 null
+     */
+    static evaluateComplexExpression(expression: string): any {
+      // 去除表达式中的空格
+      expression = expression.replace(/\s+/g, '');
+
+      /**
+       * 递归解析函数
+       * @param {string} expr - 要解析的子表达式
+       * @returns {any} 子表达式的计算结果
+       */
+      function parseExpression(expr: string): any {
+        // 处理括号内的表达式
+        while (expr.includes('(')) {
+          expr = expr.replace(/\([^()]*\)/g, match => {
+            return String(parseExpression(match.slice(1, -1)));
+          });
+        }
+
+        // 定义运算符优先级和对应的计算函数
+        const operators = [
+          { regex: /([\d.]+|\w+)\^([\d.]+|\w+)/, func: (a: any, b: any) => Math.pow(a, b) },          // 幂运算
+          { regex: /([\d.]+|\w+)\*([\d.]+|\w+)/, func: (a: number, b: number) => a * b },             // 乘法
+          { regex: /([\d.]+|\w+)\/([\d.]+|\w+)/, func: (a: number, b: number) => a / b },             // 除法
+          { regex: /([\d.]+|\w+)%([\d.]+|\w+)/, func: (a: number, b: number) => a % b },              // 求余
+          { regex: /([\d.]+|\w+)\+([\d.]+|\w+)/, func: (a: number, b: number) => a + b },             // 加法
+          { regex: /([\d.]+|\w+)-([\d.]+|\w+)/, func: (a: number, b: number) => a - b },              // 减法
+          { regex: /([\d.]+|\w+)\>([\d.]+|\w+)/, func: (a: number, b: number) => a > b },             // 大于
+          { regex: /([\d.]+|\w+)\<([\d.]+|\w+)/, func: (a: number, b: number) => a < b },             // 小于
+          { regex: /([\d.]+|\w+)==([\d.]+|\w+)/, func: (a: number, b: number) => a == b },            // 等于
+          { regex: /([\d.]+|\w+)\!=([\d.]+|\w+)/, func: (a: number, b: number) => a != b },           // 不等于
+          { regex: /([\d.]+|\w+)\>=(\d+|\w+)/, func: (a: number, b: number) => a >= b },              // 大于等于
+          { regex: /([\d.]+|\w+)\<=(\d+|\w+)/, func: (a: number, b: number) => a <= b },              // 小于等于
+          { regex: /(.+?)&&\s*(.+)/, func: (a: any, b: any) => a && b },                              // 逻辑与
+          { regex: /(.+?)\|\|\s*(.+)/, func: (a: boolean, b: boolean) => a || b },                    // 逻辑或
+          { regex: /(.+?)\!<>\s*(.+)/, func: (a: string, b: string) => a.indexOf(b) == -1 },          // 字符串不包含
+          { regex: /(.+?)<>\s*(.+)/, func: (a: string, b: string) => a.indexOf(b) !== -1 },           // 字符串包含
+        ];
+
+        // 逐个运算符处理表达式
+        for (const { regex, func } of operators) {
+          while (regex.test(expr)) {
+            expr = expr.replace(regex, (match, p1, p2) => {
+              const operand1 = isNaN(p1 as any) ? (p1 === 'true' ? true : p1 === 'false' ? false : p1) : parseFloat(p1);
+              const operand2 = isNaN(p2 as any) ? (p2 === 'true' ? true : p2 === 'false' ? false : p2) : parseFloat(p2);
+              return String(func(operand1, operand2));
+            });
+          }
+        }
+
+        // 处理逻辑非运算符
+        if (expr.startsWith('!')) {
+          const operand = expr.slice(1);
+          return !parseExpression(operand);
+        }
+
+        // 返回最终结果
+        const result = isNaN(expr as any) ? (expr === 'true' ? true : expr === 'false' ? false : expr) : parseFloat(expr);
+        return result;
+      }
+
+      // 解析并计算输入表达式
+      return parseExpression(expression);
+    }
+
+    /**
+     * 将阿拉伯数字转换为罗马数字的函数
+     * @param {number} num - 要转换的阿拉伯数字
+     * @returns {string} 对应的罗马数字
+     */
+    static intToRoman(num: number): string {
+      // 定义一个数组，包含罗马数字的数值部分
+      const val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+      // 定义一个数组，包含与数值对应的罗马数字符号
+      const syms = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+
+      // 初始化空字符串，用于存储最终的罗马数字
+      let roman = "";
+
+      // 遍历数值数组
+      for (let i = 0; i < val.length; i++) {
+        // 当输入数字大于或等于当前数值时
+        while (num >= val[i]) {
+          // 将对应的罗马数字符号添加到结果字符串中
+          roman += syms[i];
+          // 从输入数字中减去当前数值
+          num -= val[i];
+        }
+      }
+
+      // 返回最终的罗马数字字符串
+      return roman;
+    }
+
+    /**
+     * 打乱数组
+     * @param array - 需要打乱的数组。
+     * @returns 返回一个新的打乱顺序的数组。
+     */
+    static shuffleArray<T>(array: T[]): T[] {
+      // 复制输入数组，避免修改原数组
+      const shuffledArray = array.slice();
+
+      // 从数组末尾开始遍历到第二个元素
+      for (let i = shuffledArray.length - 1; i > 0; i--) {
+        // 在 0 到 i 之间随机选择一个索引
+        const j = Math.floor(Math.random() * (i + 1));
+
+        // 交换元素 shuffledArray[i] 和 shuffledArray[j]
+        [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+      }
+
+      // 返回打乱顺序后的数组
+      return shuffledArray;
+    }
+
+    /**
+     * 获取事件页名称
+     * @param eventPage 事件页
+     * @returns 
+     */
+    static getFeDataMessage(eventPage: string): string | null {
+      if (!eventPage) return null;
+      var startIdx = eventPage.indexOf(String.fromCharCode(5));
+      var feName = eventPage.slice(0, startIdx);
+      return feName ? feName : null;
+    }
+
+    /**
+     * 获得模块数据
+     * @param id 模块id
+     * @param comp 回调
+     * @param length 类别长度 默认16
+     */
+    static getModuleData(id: number, comp: Callback, length = 16) {
+      for (let i = 1; i <= length; i++) {
+        for (let j = 1; j <= GameData.getLength(id, i); j++) {
+          let d = GameData.getModuleData(id, (i - 1) * 1000 + j);
+          if (d && d.name) {
+            comp.runWith([d]);
+          }
+        }
+      }
+    }
+
+    /**
+     * 获取两个数之间的范围，包括起始和结束数字
+     * @param start - 范围的起始数字
+     * @param end - 范围的结束数字
+     * @returns 包含从起始数字到结束数字之间所有数字的数组
+     */
+    static getRange(start: number, end: number): number[] | null {
+      if (typeof start !== 'number' || typeof end !== 'number') {
+        return null
+      }
+      const range: number[] = [];
+      if (start <= end) {
+        // 如果起始数字小于或等于结束数字，按升序添加数字
+        for (let i = start; i <= end; i++) {
+          range.push(i);
+        }
+      } else {
+        // 如果起始数字大于结束数字，按降序添加数字
+        for (let i = start; i >= end; i--) {
+          range.push(i);
+        }
+      }
+      return range;
+    }
   }
 }
